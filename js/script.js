02f103a2d95c2f52769484ce0fcf293542e8c5d7
KesvaL <jabeddou@gmail.com>
2022-10-26 15:08:30 +0200

    Tests

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function loadFile(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status == 200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
+
+console.log(loadFile("devoirs/10-2022/26.md"));
